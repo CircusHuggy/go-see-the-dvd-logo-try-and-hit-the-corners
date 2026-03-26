df0bfba453bf58822a7c7b928043ca1fd0c7faa3
CircusHuggy <nimhordhamaire@gmail.com>
2026-03-26 22:50:27 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setVelocity(50, 50)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setVelocity(100, 100)
+})
 let mySprite: Sprite = null
 mySprite = sprites.createProjectileFromSprite(assets.image`DVD`, mySprite, 50, 50)
 mySprite.setStayInScreen(true)
